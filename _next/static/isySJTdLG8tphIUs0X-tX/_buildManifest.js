bfaee8cc6c5aa13ce9e4375f76a56131b4d5032f
Simon Angina <145816765+romanus-angina@users.noreply.github.com>
2025-03-23 03:33:47 -0700

    Updates

--- _next/static/isySJTdLG8tphIUs0X-tX/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-061cf4b1291f0fcc.js","static/chunks/712-c38c1f0cd4a7e164.js","static/chunks/pages/index-f26df979bc214dd0.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
