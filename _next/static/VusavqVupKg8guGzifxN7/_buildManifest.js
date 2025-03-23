b1f4bb3a240eea2d8d3180f841401cf024aa02a9
Simon Angina <145816765+romanus-angina@users.noreply.github.com>
2025-03-23 12:37:06 -0700

    Updates

---
+++ _next/static/VusavqVupKg8guGzifxN7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-061cf4b1291f0fcc.js","static/chunks/679-6affd8f61173442c.js","static/chunks/pages/index-3dbc109f2e9ffb6e.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/ZLbmyKFMncYNqV7wtATun/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-061cf4b1291f0fcc.js","static/chunks/787-198d3189afdf37ba.js","static/chunks/pages/index-cea43a1adcc84850.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
