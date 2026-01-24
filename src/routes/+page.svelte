ff0e5cf3e71c672ba044770cc31dc06ac4da95e2
Simon Angina <145816765+romanus-angina@users.noreply.github.com>
2026-01-24 16:36:01 -0600

    feat: initial app styling and link color render test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<style>
+    a {
+    color: #1a1a1a;
+    text-decoration: solid underline #84161f 3px;
+    text-underline-offset: 2px;
+}
+
+a:hover {
+    opacity: 0.8;
+    transition: ease all 0.2s;
+    color: #84161f;
+}
+</style>
